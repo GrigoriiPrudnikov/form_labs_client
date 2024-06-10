699ee36ec67211df5d06e1f7b60ec214ee033df4
Grigorii Prudnikov <griprudnikov742@gmail.com>
2024-06-10 14:11:14 +0500

    feat: add tabs for form design

--- src/app/design/page.tsx
+++ src/app/design/page.tsx
@@ -1,77 +1,26 @@
 'use client'
 
-import { DesignHeader, PreviewField, Sidebar } from '@/components'
-import { DesignField } from '@/components/designField'
-import { Button, Card, Form, ScrollArea } from '@/components/ui'
-import { useFormStore } from '@/state'
-import { getZodPrimitive } from '@/utils'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { useForm } from 'react-hook-form'
-import { z } from 'zod'
+import { DesignHeader, FormDesign, FormResponse } from '@/components'
+import { Tabs, TabsList, TabsTrigger, TabsContent } from '@/components/ui'
 
 export default function Page() {
-  const fields = useFormStore((state) => state.fields)
-  const name = useFormStore((state) => state.name)
-
-  const schema: Record<string, any> = {}
-
-  fields.map((elem) => {
-    schema[elem.id] = getZodPrimitive(elem.type, elem.isRequired)
-  })
-
-  const formSchema = z.object(schema)
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-  })
-
-  const onSubmit = (values: z.infer<typeof formSchema>) => {
-    console.log(values)
-    fields.forEach((elem) => {
-      form.setValue(elem.id, '')
-    })
-  }
-
   return (
     <>
       <DesignHeader />
-      <main className="flex gap-4 p-4">
-        {/* Sidebar */}
-        <div className="w-1/6 h-[calc(100vh-5.5rem)]">
-          <Sidebar />
+      <Tabs defaultValue="design">
+        <div className="h-14 flex justify-center items-center">
+          <TabsList>
+            <TabsTrigger value="design">Design</TabsTrigger>
+            <TabsTrigger value="response">Response</TabsTrigger>
+          </TabsList>
         </div>
-        {/* Design */}
-        <div className="w-1/2 h-[calc(100vh-5.5rem)]">
-          <Card className="w-full h-full">
-            <ScrollArea className="h-full w-full">
-              <div className="p-4 flex flex-col gap-4">
-                {fields.map((elem) => (
-                  <DesignField key={elem.id} field={elem} />
-                ))}
-              </div>
-            </ScrollArea>
-          </Card>
-        </div>
-        {/* Preview */}
-        <div className="w-1/3 h-[calc(100vh-5.5rem)]">
-          <Card className="w-full h-full">
-            <ScrollArea className="h-full w-full">
-              <Form {...form}>
-                <form className="p-4 flex flex-col gap-4" onSubmit={form.handleSubmit(onSubmit)}>
-                  <div className="text-2xl text-center">{name}</div>
-                  {fields.map((elem) => (
-                    <PreviewField key={elem.id} field={elem} form={form} />
-                  ))}
-                  {fields.length > 0 && (
-                    <Button className="w-full" type="submit">
-                      Reset
-                    </Button>
-                  )}
-                </form>
-              </Form>
-            </ScrollArea>
-          </Card>
-        </div>
-      </main>
+        <TabsContent value="design" className="mt-0">
+          <FormDesign />
+        </TabsContent>
+        <TabsContent value="response" className="mt-0">
+          <FormResponse />
+        </TabsContent>
+      </Tabs>
     </>
   )
 }
---
+++ src/components/formDesign.tsx
@@ -0,0 +1,72 @@
+import { FC } from 'react'
+import { DesignField, PreviewField, Sidebar } from '.'
+import { Button, Card, Form, ScrollArea } from './ui'
+import { useFormStore } from '@/state'
+import { getZodPrimitive } from '@/utils'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+export const FormDesign: FC = () => {
+  const fields = useFormStore((state) => state.fields)
+  const name = useFormStore((state) => state.name)
+
+  const schema: Record<string, any> = {}
+
+  fields.map((elem) => {
+    schema[elem.id] = getZodPrimitive(elem.type, elem.isRequired)
+  })
+
+  const formSchema = z.object(schema)
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+  })
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log(values)
+    fields.forEach((elem) => {
+      form.setValue(elem.id, '')
+    })
+  }
+
+  return (
+    <main className="flex gap-4 p-4 pt-0">
+      {/* Sidebar */}
+      <div className="w-1/6 h-[calc(100vh-8rem)]">
+        <Sidebar />
+      </div>
+      {/* Design */}
+      <div className="w-1/2 h-[calc(100vh-8rem)]">
+        <Card className="w-full h-full">
+          <ScrollArea className="h-full w-full">
+            <div className="p-4 flex flex-col gap-4">
+              {fields.map((elem) => (
+                <DesignField key={elem.id} field={elem} />
+              ))}
+            </div>
+          </ScrollArea>
+        </Card>
+      </div>
+      {/* Preview */}
+      <div className="w-1/3 h-[calc(100vh-8rem)]">
+        <Card className="w-full h-full">
+          <ScrollArea className="h-full w-full">
+            <Form {...form}>
+              <form className="p-4 flex flex-col gap-4" onSubmit={form.handleSubmit(onSubmit)}>
+                <div className="text-2xl text-center">{name}</div>
+                {fields.map((elem) => (
+                  <PreviewField key={elem.id} field={elem} form={form} />
+                ))}
+                {fields.length > 0 && (
+                  <Button className="w-full" type="submit">
+                    Reset
+                  </Button>
+                )}
+              </form>
+            </Form>
+          </ScrollArea>
+        </Card>
+      </div>
+    </main>
+  )
+}
